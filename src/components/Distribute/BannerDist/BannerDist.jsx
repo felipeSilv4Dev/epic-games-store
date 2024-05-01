5cb0b7f5f52ce13b0d21b602eb2f9ac2e42d3162
felipeSilv4Dev <felipesilva.developer@gmail.com>
2024-04-30 22:37:14 -0300

    commit update banner distribute

--- src/components/Distribute/BannerDist/BannerDist.jsx
+++ src/components/Distribute/BannerDist/BannerDist.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import styles from "./BannerDist.module.css";
 import gif from "../../../../public/img/assets/distribute.gif";
+import logo from "../../../../public/img/header/header-logo.png";
 import Image from "../../Components/Image/Image";
 import Button from "../../Components/Button/Button";
 
 const BannerDist = () => {
   return (
-    <section className={styles.container}>
-      <div>
-        <span className={styles.overlay}></span>
+    <section className={styles.container + " flex"}>
+      <div className={styles.picture}>
         <Image src={gif} alt="image Gif" />
       </div>
 
       <div className={styles.content + " flex"}>
+        <img className={styles.logo} src={logo} alt="logo" />
         <h2>Já disponível para todos os desenvolvedores e editoras</h2>
         <span className={styles.btn}>
           <Button btn="primary" theme="var(--type-2-light)">
